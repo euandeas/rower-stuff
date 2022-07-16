5c167e775f04592e19aabfbe340b1db576abcf60
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-07-16 23:42:42 +0100

    Got basics of nav to work

---
+++ RowerStuff/Fragments/PaceFragment.cs
@@ -0,0 +1,23 @@
+﻿using Android.Views;
+using Fragment = AndroidX.Fragment.App.Fragment;
+
+
+namespace RowerStuff.Fragments
+{
+    public class PaceFragment : Fragment
+    { 
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragment_pace, container, false);
+
+
+            return view;
+        }
+    }
+}
