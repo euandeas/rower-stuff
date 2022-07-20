9500c23959ea3f6b4775741b20fd607f4f354ff0
Euan <33450935+euandeas@users.noreply.github.com>
2022-07-20 19:03:01 +0100

    Simplification and preparation

---
+++ RowerStuff/DataProcessing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowerStuff
+{
+    static class DataProcessing
+    {
+
+        public static TimeSpan ParseMinSecMS(string inputMins, string inputSecs)
+        {
+            string minutes = inputMins;
+            string seconds = inputSecs;
+
+            if (string.IsNullOrWhiteSpace(inputMins))
+                minutes = "00";
+
+            if (string.IsNullOrWhiteSpace(inputSecs) || inputSecs == ".")
+                seconds = "00";
+
+            long minutesParsed = long.Parse(minutes);
+            double secondsParsed = double.Parse(seconds, CultureInfo.InvariantCulture);
+
+            return TimeSpan.FromMinutes(minutesParsed).Add(TimeSpan.FromSeconds(secondsParsed));
+        }
+
+    }
+}
