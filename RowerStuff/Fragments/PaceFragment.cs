db7c60a33bb7666a6cfc0ed33fd9c2d40c4467ea
Euan <33450935+euandeas@users.noreply.github.com>
2022-07-20 22:20:19 +0100

    Toolbar and drawerlayout setup finalised

--- RowerStuff/Fragments/PaceFragment.cs
+++ RowerStuff/Fragments/PaceFragment.cs
@@ -1,11 +1,13 @@
 ﻿using Android.Views;
+using AndroidX.AppCompat.View.Menu;
+using Google.Android.Material.AppBar;
 using Fragment = AndroidX.Fragment.App.Fragment;
 
 
 namespace RowerStuff.Fragments
 {
     public class PaceFragment : Fragment
-    { 
+    {
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,8 +18,29 @@ namespace RowerStuff.Fragments
         {
             View view = inflater.Inflate(Resource.Layout.fragment_pace, container, false);
 
+            MaterialToolbar toolbar = view.FindViewById<MaterialToolbar>(Resource.Id.toolbar);
+            (Activity as MainActivity).SetupToolBar(toolbar, "Pace");
+            HasOptionsMenu = true;
 
             return view;
+        }
+
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            inflater.Inflate(Resource.Menu.toolbar, menu);
+
+            if (menu is MenuBuilder)
+            {
+                MenuBuilder m = (MenuBuilder)menu;
+                m.SetOptionalIconsVisible(true);
+            }
+
+            base.OnCreateOptionsMenu(menu, inflater);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
--- RowerStuff/MainActivity.cs
+++ RowerStuff/MainActivity.cs
@@ -1,12 +1,20 @@
 using Android.Graphics;
 using AndroidX.AppCompat.App;
 using AndroidX.DrawerLayout.Widget;
+using AndroidX.Navigation.Fragment;
+using AndroidX.Navigation.UI;
+using AndroidX.Navigation;
+using Google.Android.Material.Navigation;
+using Google.Android.Material.AppBar;
 
 namespace RowerStuff
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private NavController navController;
+        private AppBarConfiguration appBarConfiguration;
+
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -16,6 +24,17 @@ namespace RowerStuff
 
             DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
             drawer.SetStatusBarBackgroundColor(Color.Transparent);
+
+            navController = ((NavHostFragment)SupportFragmentManager.FindFragmentById(Resource.Id.nav_host_fragment)).NavController;
+            appBarConfiguration = new AppBarConfiguration.Builder(navController.Graph).SetDrawerLayout(drawer).Build();
+        }
+
+        public void SetupToolBar(MaterialToolbar toolbar, string title)
+        {
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title=title;
+
+            NavigationUI.SetupWithNavController(toolbar, navController, appBarConfiguration);
         }
     }
 }
