f6ee54d31a496fc8fcabb100168ee40c5e1afae8
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-07-30 23:19:45 +0100

    Added preferences

--- RowerStuff/MainActivity.cs
+++ RowerStuff/MainActivity.cs
@@ -1,9 +1,12 @@
+using Android.Content;
 using Android.Graphics;
+using Android.OS;
 using AndroidX.AppCompat.App;
 using AndroidX.DrawerLayout.Widget;
 using AndroidX.Navigation;
 using AndroidX.Navigation.Fragment;
 using AndroidX.Navigation.UI;
+using AndroidX.Preference;
 using Google.Android.Material.AppBar;
 using Google.Android.Material.Navigation;
 using SplashScreenX = AndroidX.Core.SplashScreen.SplashScreen;
@@ -19,6 +22,23 @@ namespace RowerStuff
 
         protected override void OnCreate(Bundle? savedInstanceState)
         {
+            if (savedInstanceState == null)
+            {
+                ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
+                switch (prefs.GetString("theme_preference", "sysdefault"))
+                {
+                    case "light":
+                        base.Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
+                        break;
+                    case "dark":
+                        base.Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightYes);
+                        break;
+                    case "sysdefault":
+                        base.Delegate.SetLocalNightMode((Build.VERSION.SdkInt >= BuildVersionCodes.Q) ? AppCompatDelegate.ModeNightFollowSystem : AppCompatDelegate.ModeNightAutoBattery);
+                        break;
+                }
+            }
+
             SplashScreenX.InstallSplashScreen(this);
 
             base.OnCreate(savedInstanceState);
@@ -39,9 +59,11 @@ namespace RowerStuff
                 Resource.Id.steadyStateFragment,
                 Resource.Id.pacePredictionFragment,
                 Resource.Id.vo2MaxFragment,
-                Resource.Id.rateFragment
+                Resource.Id.rateFragment,
+                Resource.Id.settingsFragment
                 ).SetOpenableLayout(drawer).Build();
 
+            NavigationUI.SetupWithNavController(navView, navController);
         }
 
         public void SetupToolBar(MaterialToolbar toolbar)
@@ -50,7 +72,7 @@ namespace RowerStuff
             SupportActionBar.Title = "Pace";
 
             NavigationUI.SetupWithNavController(toolbar, navController, appBarConfiguration);
-            NavigationUI.SetupWithNavController(navView, navController);
         }
+
     }
 }
