24ea97546ac6742e71299b63c2fdde6dc106d887
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-07-30 17:56:35 +0100

    Completed watts and weight adjustment

--- RowerStuff/Fragments/WattsFragment.cs
+++ RowerStuff/Fragments/WattsFragment.cs
@@ -1,6 +1,8 @@
 ﻿using Android.Views;
 using AndroidX.AppCompat.View.Menu;
 using Google.Android.Material.AppBar;
+using RowerStuff.Models;
+using System.Globalization;
 using Fragment = AndroidX.Fragment.App.Fragment;
 
 
@@ -8,6 +10,9 @@ namespace RowerStuff.Fragments
 {
     public class WattsFragment : Fragment
     {
+        private EditText enteredMin;
+        private EditText enteredSec;
+        private EditText enteredWatts;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -22,7 +27,56 @@ namespace RowerStuff.Fragments
             (Activity as MainActivity).SetupToolBar(toolbar);
             HasOptionsMenu = true;
 
+            enteredMin = view.FindViewById<EditText>(Resource.Id.enteredMin);
+            enteredSec = view.FindViewById<EditText>(Resource.Id.enteredSec);
+
+            enteredWatts = view.FindViewById<EditText>(Resource.Id.enteredWatts);
+
+            Button calculateButton = view.FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += CalculateButton_Click;
+            calculateButton.LongClick += CalculateButton_LongClick;
+
             return view;
+        }
+
+        private void CalculateButton_LongClick(object? sender, View.LongClickEventArgs e)
+        {
+            enteredMin.Text = "";
+            enteredSec.Text = "";
+            enteredWatts.Text = "";
+        }
+
+        private void CalculateButton_Click(object? sender, EventArgs e)
+        {
+            //Calculate Watts - watts = 2.8/(split/500)³
+            if ((enteredWatts.Text == "") && (enteredMin.Text != "" || enteredSec.Text != ""))
+            {
+                if (Helpers.ParseMinSecMS(enteredMin.Text, enteredSec.Text) is TimeSpan parsedSplitTime && parsedSplitTime.TotalSeconds == 0)
+                {
+                    Toast.MakeText(Activity, "Split has no value!", ToastLength.Short).Show();
+                    return;
+                }
+
+                enteredWatts.Text = $"{Formulas.PaceToWatts(parsedSplitTime)}";
+            }
+            //Calculate Split - split = (cube-root(w*2.8))*500
+            else if ((enteredWatts.Text != "" && (enteredWatts.Text != "." || enteredWatts.Text != ",")) && (enteredMin.Text == "" && enteredSec.Text == ""))
+            {
+                if (double.Parse(enteredWatts.Text, CultureInfo.InvariantCulture) is double watts && watts == 0)
+                {
+                    Toast.MakeText(Activity, "Watts has no value!", ToastLength.Short).Show();
+                    return;
+                }
+
+                TimeSpan splitReadable = Formulas.WattsToPace(watts);
+
+                enteredMin.Text = $"{(int)splitReadable.TotalMinutes}";
+                enteredSec.Text = $"{splitReadable.Seconds:D2}.{splitReadable.Milliseconds}";
+            }
+            else
+            {
+                Toast.MakeText(Activity, "Must enter either split or watts!", ToastLength.Short).Show();
+            }
         }
 
         public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
@@ -41,7 +95,7 @@ namespace RowerStuff.Fragments
         {
             if (item.ItemId == Resource.Id.info)
             {
-                Helpers.HelpDialog(Activity, "Rate", "");
+                Helpers.HelpDialog(Activity, "Watts", "Enter either splits or watts and it will be converted to the other.");
             }
 
             return base.OnOptionsItemSelected(item);
--- RowerStuff/Fragments/WeightAdjustmentFragment.cs
+++ RowerStuff/Fragments/WeightAdjustmentFragment.cs
@@ -1,6 +1,9 @@
 ﻿using Android.Views;
 using AndroidX.AppCompat.View.Menu;
+using AndroidX.CardView.Widget;
 using Google.Android.Material.AppBar;
+using RowerStuff.Models;
+using System.Globalization;
 using Fragment = AndroidX.Fragment.App.Fragment;
 
 
@@ -8,6 +11,13 @@ namespace RowerStuff.Fragments
 {
     public class WeightAdjustmentFragment : Fragment
     {
+        private EditText enteredBodyWeight;
+        private Spinner weightSpinner;
+        private EditText enteredMin;
+        private EditText enteredSec;
+        private EditText enteredDistance;
+        private TextView resultLabel;
+        private TextView result;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -22,7 +32,97 @@ namespace RowerStuff.Fragments
             (Activity as MainActivity).SetupToolBar(toolbar);
             HasOptionsMenu = true;
 
+            enteredBodyWeight = view.FindViewById<EditText>(Resource.Id.enteredBodyWeight);
+            weightSpinner = view.FindViewById<Spinner>(Resource.Id.weightUnitSpinner);
+            ArrayAdapter wAdapter = ArrayAdapter.CreateFromResource(view.Context, Resource.Array.weight_units_array, Android.Resource.Layout.SimpleSpinnerItem);
+            wAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            weightSpinner.Adapter = wAdapter;       
+
+            enteredMin = view.FindViewById<EditText>(Resource.Id.enteredMin);
+            enteredSec = view.FindViewById<EditText>(Resource.Id.enteredSec);
+            
+            enteredDistance = view.FindViewById<EditText>(Resource.Id.enteredDistance);
+
+            resultLabel = view.FindViewById<TextView>(Resource.Id.adjustedResultLabel);
+            result = view.FindViewById<TextView>(Resource.Id.adjustedResult);
+
+            Button calculateButton = view.FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += CalculateButton_Click;
+            calculateButton.LongClick += CalculateButton_LongClick;
+
             return view;
+        }
+
+        private void CalculateButton_LongClick(object? sender, View.LongClickEventArgs e)
+        {
+            enteredBodyWeight.Text = "";
+            enteredMin.Text = "";
+            enteredSec.Text = "";
+            enteredDistance.Text = "";
+            result.Text = "";
+        }
+
+        private void CalculateButton_Click(object? sender, EventArgs e)
+        {
+            if (enteredBodyWeight.Text != "" && enteredBodyWeight.Text != "." && enteredBodyWeight.Text != ".")
+            {
+                double weight;
+
+                if (weightSpinner.SelectedItem.ToString() == "kg")
+                {
+                    if (double.Parse(enteredBodyWeight.Text, CultureInfo.InvariantCulture) is double weightkg && weightkg == 0)
+                    {
+                        Toast.MakeText(Activity, "Weight has no value!", ToastLength.Short).Show();
+                        return;
+                    }
+
+                    weight = Formulas.KGToLB(weightkg);
+                }
+                else // if lb selected
+                {
+                    if (double.Parse(enteredBodyWeight.Text, CultureInfo.InvariantCulture) is double weightlb && weightlb == 0)
+                    {
+                        Toast.MakeText(Activity, "Weight has no value!", ToastLength.Short).Show();
+                        return;
+                    }
+
+                    weight = weightlb;
+                }
+
+                //Corrected time = Wf x actual time(seconds)
+                if ((enteredMin.Text != "" || enteredSec.Text != "") && (enteredDistance.Text == ""))
+                {
+                    if (Helpers.ParseMinSecMS(enteredMin.Text, enteredSec.Text) is TimeSpan parsedSplitTime && parsedSplitTime.TotalSeconds == 0)
+                    {
+                        Toast.MakeText(Activity, "Split has no value!", ToastLength.Short).Show();
+                        return;
+                    }
+
+                    TimeSpan timeReadable = Formulas.TimeWeightAdjusted(weight, parsedSplitTime);
+                    resultLabel.Text = "Adjusted Time";
+                    result.Text = string.Format("{0}:{1}.{2}", (int)timeReadable.TotalMinutes, timeReadable.Seconds, timeReadable.Milliseconds);
+                }
+                //Corrected distance = actual distance / Wf
+                else if ((enteredDistance.Text != "") && (enteredMin.Text == "" && enteredSec.Text == ""))
+                {
+                    if (int.Parse(enteredDistance.Text) is int distance && distance == 0)
+                    {
+                        Toast.MakeText(Activity, "Distance has no value!", ToastLength.Short).Show();
+                        return;
+                    }
+
+                    resultLabel.Text = "Adjusted Distance";
+                    result.Text = Formulas.DistanceWeightAdjusted(weight, distance).ToString();
+                }
+                else
+                {
+                    Toast.MakeText(Activity, "Must enter either Time Or Distance.", ToastLength.Short).Show();
+                }
+            }
+            else
+            {
+                Toast.MakeText(Activity, "Must enter body weight!", ToastLength.Short).Show();
+            }
         }
 
         public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
@@ -41,7 +141,7 @@ namespace RowerStuff.Fragments
         {
             if (item.ItemId == Resource.Id.info)
             {
-                Helpers.HelpDialog(Activity, "Rate", "");
+                Helpers.HelpDialog(Activity, "Weight Adjustment", "Enter a body weight and then enter either the total time or distance of a piece. The adjusted result corresponding to what you entered will be returned.");
             }
 
             return base.OnOptionsItemSelected(item);
